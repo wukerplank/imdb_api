90a412ddece5e94ce64da37b9df521675be55e04
Christoph Edthofer <wukerplank@edthofer.at>
2022-11-09 07:35:55 +0100

    Use embedded data to retrieve name

--- lib/imdb_api/person.rb
+++ lib/imdb_api/person.rb
@@ -3,9 +3,9 @@ module ImdbApi
 
     def self.find(imdb_id)
       doc = CacheFile.person_data(imdb_id)
-
-      name = doc.at('h1.header').at("span.itemprop").inner_html.strip
-      name = name.gsub(/^(.*)\s+(\<.*?\>.*?\<\/.*?\>)?$/, "\\1")
+      raw_data = doc.at("script[type='application/ld+json']")
+      data = JSON.parse(raw_data)
+      name = data["name"]
 
       return {imdb_id: imdb_id, name: name}
     end
